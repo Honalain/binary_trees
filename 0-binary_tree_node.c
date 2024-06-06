4fbc9b5067f3f3f28343b4ed51039c6247b4d2c8
root <root@Nyirinkwaya>
2024-06-06 23:31:56 +0300

    This is task for ALX project on 0x1D. C - Binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,17 +4,16 @@
 /**
  * binary_tree_node - function that create bionary tree
  * @parent: pointer to the parent node of the node to create
- * @value: Value to put in the new node.
- *
+ * @value: Value to put in the new node
  * Return: return a pointer to the new node or null if failure
- 
+ *
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root_node = malloc(sizeof(binary_tree_t));
 
-	if(!root_node)
+	if (!root_node)
 		return (NULL);
 
 	root_node->n = value;
